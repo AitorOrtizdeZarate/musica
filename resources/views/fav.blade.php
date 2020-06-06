2b8aa733b76a60df6d19cf5966f4f39913ca8154
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2020-06-06 12:58:56 +0200

    estilos de la pagina de favoritos

--- resources/views/fav.blade.php
+++ resources/views/fav.blade.php
@@ -5,10 +5,36 @@
    <h1 style="font-size: 70px;">Cantantes favoritos</h1>
 
    @foreach($users->cantante as $cantante)
-   	<p>{{$cantante->nombre}}</p>
-   @endforeach
+   	<div class="d-flex justify-content-center">
+   		<div class="d-flex justify-content-between align-items-center bg-dark flex-row border border-secondary rounded text-white col-6" style="height: 100px;">
+		  <div class="" >
+		  	<img src="/{{$cantante->imagen}}" style="height: 90px;" class="rounded-circle ">
+		  </div>
+		  <div class="p-2 col-2">
+		  	<h4>{{$cantante->nombre}}</h4>
+		  </div>
+		  <div class="p-2 col-2 d-flex align-items-center">
+		  	<h5 >{{count($cantante->album)}} albums</h5>
+		  </div>
+		  <div class="p-2">
+		  	<a href="{{route('cantante.show', $cantante->id)}}" class="btn btn-info btn-block">Ver Cantante</a>
+		  </div>
+		</div>
+   	</div>
+   	@endforeach
+   
    
    
 
-   
 @endsection
+
+<!-- <div class="border border-black row col-6 bg-white justify-content-between align-items-center">
+	   		<div class="">
+	   			<img src="/{{$cantante->imagen}}" style="height: 100px;" >
+	   		</div>
+	   		<div>
+	   			<h4>{{$cantante->nombre}}</h4>
+	   		</div>
+	   		
+	   		
+	   	</div>-->
